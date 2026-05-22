3f3715f00d1dc66b48ba68df0b5ba13fcdb3a09f
Trevor McCulloch <trevor.mcculloch@mongodb.com>
2026-05-22 13:56:00 -0700

    Arc because Lucene is wildly underspecified on Directory.close()

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,11 +1,16 @@
 use std::path::PathBuf;
+use std::sync::Arc;
 
 #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
 struct CacheKey {
-    file_ord: usize,
-    offset: usize,
+    file: usize,
+    page: usize,
 }
 
+/// The underlying native implementation of the Java FoyerDirectory class.
+///
+/// This is structured as a native class to avoid caching lots of data on the Java heap, which also
+/// forces us to handle IO details to provide read-through and write-through cache.
 pub struct FoyerDirectory {
     path: PathBuf,
     cache: foyer::Cache<CacheKey, Box<[u8]>>,
@@ -39,14 +44,14 @@ pub unsafe extern "C" fn foyer_open_directory(
     path_len: u64,
     cache_bytes: u64,
     log_page_size: i32,
-) -> *mut FoyerDirectory {
+) -> *const FoyerDirectory {
     let path_bytes = unsafe { std::slice::from_raw_parts(path, path_len as usize) };
     let path = PathBuf::from(std::str::from_utf8(path_bytes).unwrap());
     let page_size = 1u32 << log_page_size;
-    Box::into_raw(Box::new(FoyerDirectory::new(path, cache_bytes, page_size)))
+    Arc::into_raw(Arc::new(FoyerDirectory::new(path, cache_bytes, page_size)))
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn foyer_close_directory(dir: *mut FoyerDirectory) {
-    let _ = unsafe { Box::from_raw(dir) };
+pub unsafe extern "C" fn foyer_close_directory(dir: *const FoyerDirectory) {
+    let _ = unsafe { Arc::from_raw(dir) };
 }
